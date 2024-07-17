bf5b82421613e605db33d878265e462903b00acd
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-07-17 20:46:06 +0700

    Component States

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import List from "./List";
 import Item from "./Item";
+import { useState } from "react";
 
 export default function App() {
+  const [data, setData] = useState([
+    { id: 1, content: "Hello, World!", name: "Alice" },
+    { id: 2, content: "React is fun.", name: "Bob" },
+    { id: 3, content: "Yay, interesting.", name: "Chris" },
+  ]);
+
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>Yaycha</h1>
       <List>
-        <Item content="Hello, World!" name="Alice" />
-        <Item content="React is fun." name="Bob" />
+        {data.map((item) => (
+          <Item key={item.id} content={item.content} name={item.name} />
+        ))}
       </List>
     </div>
   );

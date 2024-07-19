dafe880d5e67234d45c03159452d9df5083f2f36
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-07-19 14:49:22 +0700

    Building Form and Adding Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import List from "./List";
 import Item from "./Item";
 import { useState } from "react";
+import Form from "./Form";
 
 export default function App() {
   const [data, setData] = useState([
@@ -9,13 +10,19 @@ export default function App() {
     { id: 3, content: "Yay, interesting.", name: "Chris" },
   ]);
 
-  const remove = id => {
-    setData(data.filter(item => item.id !== id));
-  }
+  const remove = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, content, name }]);
+  };
 
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
       <h1>Yaycha</h1>
+      <Form add={add} />
       <List>
         {data.map((item) => (
           <Item key={item.id} item={item} remove={remove} />

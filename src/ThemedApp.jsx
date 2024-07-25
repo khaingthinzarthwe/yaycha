7b13123eac1e1e3fa2078b0a0944edbf894ea812
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-07-25 14:33:15 +0700

    Building AppDrawer

--- src/ThemedApp.jsx
+++ src/ThemedApp.jsx
@@ -1,6 +1,11 @@
 import { createContext, useState, useContext, useMemo } from "react";
 import App from "./App";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import AppDrawer from "./components/AppDrawer";
+import {
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import { deepPurple, grey } from "@mui/material/colors";
 
 const AppContext = createContext();
@@ -9,24 +14,40 @@ export function useApp() {
 }
 
 export default function ThemedApp() {
+  const [showDrawer, setShowDrawer] = useState(true);
   const [showForm, setShowForm] = useState(false);
+  const [auth, setAuth] = useState(null);
   const [mode, setMode] = useState("dark");
 
   const theme = useMemo(() => {
     return createTheme({
-      palette: { mode },
-      primary: deepPurple,
-      banner: mode === "dark" ? grey[800] : grey[200],
-      text: {
-        fade: grey[500],
+      palette: {
+        mode,
+        primary: deepPurple,
+        banner: mode === "dark" ? grey[800] : grey[200],
+        text: {
+          fade: grey[500],
+        },
       },
     });
   }, [mode]);
 
   return (
     <ThemeProvider theme={theme}>
-      <AppContext.Provider value={{ showForm, setShowForm, mode, setMode }}>
+      <AppContext.Provider
+        value={{
+          showDrawer,
+          setShowDrawer,
+          showForm,
+          setShowForm,
+          auth,
+          setAuth,
+          mode,
+          setMode,
+        }}
+      >
         <App />
+        <AppDrawer />
         <CssBaseline />
       </AppContext.Provider>
     </ThemeProvider>

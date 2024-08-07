599ca2f0b9ce7d5df72bc05cd29d4bdfddf189fb
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-08-07 13:06:50 +0700

    Environment Variable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import Form from "../components/Form";
 import Item from "../components/Item";
@@ -6,11 +6,14 @@ import { useApp } from "../ThemedApp";
 
 export default function Home() {
   const { showForm, setGlobalMsg } = useApp();
-  const [data, setData] = useState([
-    { id: 3, content: "Yay, interesting.", name: "Chris" },
-    { id: 2, content: "React is fun.", name: "Bob" },
-    { id: 1, content: "Hello, World!", name: "Alice" },
-  ]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const api = import.meta.env.VITE_API;
+    fetch(`${api}/content/posts`).then(async res => {
+      setData(await res.json());
+    });
+  }, [])
 
   const remove = (id) => {
     setData(data.filter((item) => item.id !== id));

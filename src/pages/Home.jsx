3212ede16e471964ab8def933c7c0996c913632c
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-08-07 13:56:37 +0700

    Data Fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,19 @@
-import { useState, useEffect } from "react";
 import { Box, Alert } from "@mui/material";
 import Form from "../components/Form";
 import Item from "../components/Item";
 import { useApp } from "../ThemedApp";
+import { useQuery } from "react-query";
+
+const api = import.meta.env.VITE_API;
 
 export default function Home() {
   const { showForm, setGlobalMsg } = useApp();
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    const api = import.meta.env.VITE_API;
-    fetch(`${api}/content/posts`)
-      .then(async (res) => {
-        if (res.ok) {
-          setData(await res.json());
-          setLoading(false);
-        } else {
-          setError(true);
-        }
-      })
-      .catch(() => {
-        setError(true);
-      });
-  }, []);
+  const { isLoading, isError, error, data, setData } = useQuery("posts",
+    async () => {
+      const res = await fetch(`${api}/content/posts`);
+      return res.json();
+    }
+  );
 
   const remove = (id) => {
     setData(data.filter((item) => item.id !== id));
@@ -36,15 +25,15 @@ export default function Home() {
     setGlobalMsg("An item added");
   };
 
-  if (error) {
+  if (isError) {
     return (
       <Box>
-        <Alert severity="warning">Cannot Fetch Data</Alert>
+        <Alert severity="warning">{error.message}</Alert>
       </Box>
     );
   }
 
-  if (loading) {
+  if (isLoading) {
     return <Box sx={{ textAlign: "center" }}>Loading...</Box>;
   }
 

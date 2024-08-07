c4dd8d61440bfdf2195fd28bd9a68eaf59e2a8f0
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-08-07 14:08:40 +0700

    Remove Data from Home Page with useMutation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,26 +2,38 @@ import { Box, Alert } from "@mui/material";
 import Form from "../components/Form";
 import Item from "../components/Item";
 import { useApp } from "../ThemedApp";
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
+import { queryClient } from "../ThemedApp";
 
 const api = import.meta.env.VITE_API;
 
 export default function Home() {
   const { showForm, setGlobalMsg } = useApp();
-  const { isLoading, isError, error, data, setData } = useQuery("posts",
+  const { isLoading, isError, error, data } = useQuery(
+    "posts",
     async () => {
       const res = await fetch(`${api}/content/posts`);
       return res.json();
     }
   );
 
-  const remove = (id) => {
-    setData(data.filter((item) => item.id !== id));
-    setGlobalMsg("An item deleted");
-  };
-  const add = (content, name) => {
-    const id = data[0].id + 1;
-    setData([{ id, content, name }, ...data]);
+  const remove = useMutation(
+    async (id) => {
+      await fetch(`${api}/content/posts/${id}`, {
+        method: "DELETE",
+      });
+    },
+    {
+      onMutate: (id) => {
+        queryClient.cancelQueries("posts");
+        queryClient.setQueryData("posts", (old) =>
+          old.filter((item) => item.id !== id)
+        );
+        setGlobalMsg("A post deleted");
+      },
+    }
+  );
+  const add = () => {
     setGlobalMsg("An item added");
   };
 
@@ -41,7 +53,7 @@ export default function Home() {
     <Box>
       {showForm && <Form add={add} />}
       {data.map((item) => {
-        return <Item key={item.id} item={item} remove={remove} />;
+        return <Item key={item.id} item={item} remove={remove.mutate} />;
       })}
     </Box>
   );
